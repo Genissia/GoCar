ea077c4e49a1c7a3afc5cca47568995bf3f9adc4
genissia <genissia.yasser1@gmail.com>
2019-09-28 00:20:28 +0200

    Milestone one

--- app/src/main/java/com/example/gocar/app/AppConfig.java
+++ app/src/main/java/com/example/gocar/app/AppConfig.java
@@ -3,9 +3,9 @@ package com.example.gocar.app;
 public class AppConfig
 {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.43.221/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.1.8/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.43.221/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.1.8/android_login_api/register.php";
 }
 
